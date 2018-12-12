ec57a50b1066e3610ed4e9caa5e410a550a4a708
StevenJBurns <stevenjburns@msn.com>
2018-12-12 00:02:43 -0700

    Started middleware with routers to /api and /account

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-
+/* Node & Express Dependencies */
+const path = "path";
 const express = require("express");
 
+/* Local Dependencies */
+const routerAPI = require("./routing/routerAPI");
+const routerAccount = require("./routing/routerUser");
+
+
+/* Create the Express server object */
 const server = new express();
 
+/* Start middleware for annoying favicon GET requests */ 
 
-server.listen(process.env.PORT || 8000, _ => console.log("HI"));
+/* Start Middleware with local routers */
+server.use("/account", routerAccount);
+server.use("/api", routerAPI);
+
+server.listen(process.env.PORT || 8000, () => console.log("HI"));
+
+module.exports = server;
