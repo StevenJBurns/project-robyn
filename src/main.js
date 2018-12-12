f44c346ee4b3053b931352a0c0bcc15e1dc3a390
StevenJBurns <stevenjburns@msn.com>
2018-12-11 23:08:42 -0700

    Tweaked public index and main.js to my personal style

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+/* Vue, Vuex and Router Dependencies */
+import Vue from "vue";
+import store from "./store";
+import router from "./router";
+
+/* Local Dependencies */
+import App from "./App.vue";
+
+/* Service Worker for PWA */
+import "./registerServiceWorker";
+
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+new Vue({ router, store, render: h => h(App) }).$mount('#vue-root');
