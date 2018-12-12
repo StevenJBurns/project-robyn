4e22c9ece9dba26653c1baf9cdfdd5a8da930839
StevenJBurns <stevenjburns@msn.com>
2018-12-12 16:57:06 -0700

    Added Morgan logging to server.js middleware

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const path = require("path");
 const express = require("express");
 
+/* External Dependencies */
+const logger = require("morgan");
+
 /* Local Dependencies */
 const routerAPI = require("./routing/routerAPI.js");
 const routerAccount = require("./routing/routerAccount.js");
@@ -12,10 +15,14 @@ require("dotenv").config();
 /* Create a port and the Express server object */
 const port = process.env.PORT || 4000;
 const server = new express();
+console.clear();
 
 /* Start middleware */
 
-/* use Express v4.16 built in body-parser */
+/* Morgan Middleware for logging */
+server.use(logger("dev"));
+
+/* use Express v4.16.4 built in body-parser */
 server.use(express.json());
 
 /* use serve-favicon for annoying favicon GET requests */ 
