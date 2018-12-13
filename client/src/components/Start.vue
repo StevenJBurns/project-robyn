5d1df19c2f0fc9513aed3151ed0c38abd1d2e5a5
StevenJBurns <stevenjburns@msn.com>
2018-12-13 16:37:06 -0700

    Created Start.vue and Finish.vue to begin app flow of questions and results

---
+++ client/src/components/Start.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>How Are You Feeling?</h3>
+    <ul>
+      <li v-for="mood in moods" :key="mood['title']">{{mood['title']}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import moods from "../data/moods.json";
+
+  // moods.forEach(mood => console.log(mood));
+
+  export default {
+    data: function() {
+      return {
+        colors: ["Blue", "Yellow", "Red"],
+        moods,
+        currentMood: null
+      }
+    },
+    methods: {
+      // setMood() {},
+      // setResponse() {}
+    }
+  }
+</script>
+
+<style scoped></style>
