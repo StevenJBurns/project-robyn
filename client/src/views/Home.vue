af1ec20a9fcd1f7eab3ff98513440890301cb172
StevenJBurns <stevenjburns@msn.com>
2018-12-13 18:51:42 -0700

    Added boilerplate v-responsive component to Home.vue

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-responsive>
+    <v-container fill-height>
+      <v-layout align-center>
+        <v-flex>
+          <h3 class="display-2">Welcome to the Robyn's Project</h3>
+          <span class="subheading">Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum.</span>
+          <v-divider class="my-3"></v-divider>
+          <div class="title mb-3">Check out our newest features!</div>
+          <v-btn class="mx-0" color="primary" large>See more</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-responsive>
 </template>
 
 <script>
